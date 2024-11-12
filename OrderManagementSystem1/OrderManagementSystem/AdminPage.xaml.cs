c4262b225661e78b9e746002a93b942564ca8cdd
Visiliki <saifmohamed.0523034@gmail.com>
2024-11-12 08:27:27 +0200

    Last update

---
+++ OrderManagementSystem1/OrderManagementSystem/AdminPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Page
+    {
+        cashierEntities DB = new cashierEntities();
+        public AdminPage()
+        {
+            InitializeComponent();
+            show();
+        }
+
+        public void show()
+        {
+            ProductsGrid.ItemsSource = DB.Products.ToList();
+
+        }
+
+        private void add_btn(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product();
+
+            product.ProductName = txt1.Text;
+            product.Price = int.Parse(txt2.Text);
+
+            DB.Products.Add(product);
+            DB.SaveChanges();
+            show();
+
+        }
+
+        private void delete_btn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+
+                Product product = new Product();
+                var seleted = ProductsGrid.SelectedItem as Product;
+
+                product = DB.Products.Remove(seleted);
+
+                DB.SaveChanges();
+                show();
+
+
+            }
+            catch (Exception es)
+            {
+
+                MessageBox.Show(es.Message);
+
+
+            }
+
+        }
+
+        private void selected1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+       
+    }
+
+
+
+
+}
---
+++ OrderManagementSystem1/OrderManagementSystem/UserPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OrderManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Page
+    {
+
+        cashierEntities DB = new cashierEntities();
+
+        public UserPage()
+        {
+            InitializeComponent();
+            show();
+            show2();
+        }
+
+        public void show()
+        {
+            ProductList.ItemsSource = DB.Products.ToList();
+
+        }
+
+        public void show2()
+        {
+            OrderList.ItemsSource = Hi();
+
+        }
+
+        private void AddOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+
+                Product product = new Product();
+                Order order = new Order();
+                int x = int.Parse(txt3.Text);
+                Products1 p2 = new Products1();
+
+                p2 = Hi().FirstOrDefault(o => o.ProductId == x);
+
+                MessageBox.Show(p2.Quantity.ToString());
+
+                p2.Quantity = p2.Quantity + 1;
+
+                show2();
+
+
+
+                Hi().Add(p2);
+
+                DB.SaveChanges();
+                show2();
+
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+
+            }
+
+
+
+        }
+
+
+        public List<Products1> Hi()
+        {
+
+            Product p1 = new Product();
+            Order order = new Order();
+            List<Products1> ss = new List<Products1>();
+
+            string size = DB.Products.ToList().Capacity.ToString();
+            int size1 = int.Parse(size);
+
+
+
+            for (int i = 1; i <= size1; i++)
+            {
+
+                p1 = DB.Products.FirstOrDefault(a => a.ProductId == i);
+
+                if (p1 != null)
+                {
+
+                    order = DB.Orders.FirstOrDefault(d => d.ProductId == p1.ProductId);
+
+                    if (order != null)
+                    {
+
+
+                        ss.Add(new Products1
+                        {
+                            OrderId = order.OrderId,
+                            ProductId = p1.ProductId,
+                            Quantity = order.Quantity,
+                            Product = p1.ProductName
+
+                        });
+
+
+                    }
+
+                }
+
+            }
+
+            return ss;
+
+        }
+
+        private void DeleteOrderBut_Click(object sender, RoutedEventArgs e)
+        {
+
+            Order order = new Order();
+            var seleted = OrderList.SelectedItem as Order;
+
+            order = DB.Orders.Remove(seleted);
+
+            DB.SaveChanges();
+            show2();
+        }
+    }
+
+
+
+    public class Products1
+    {
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Product { get; set; }
+
+
+
+    }
+}
